fc11fdb0dcb864138d5c7d1bdd6882a02a178eaf
Moumita <alispower77@gmail.com>
2025-11-29 13:20:11 +0600

    Add debugging logs to chat routes

--- routes/chat.js
+++ routes/chat.js
@@ -9,11 +9,18 @@ const {
 } = require('../controllers/chatController');
 const { protect } = require('../middleware/auth');
 
+console.log('🔄 Chat routes loaded');
+
 // Protected routes
 router.get('/', protect, getChats);
+console.log('🔄 Registered GET / route');
 router.post('/order/:orderId', protect, createOrderChat);
+console.log('🔄 Registered POST /order/:orderId route');
 router.get('/:id', protect, getChatById);
+console.log('🔄 Registered GET /:id route');
 router.post('/:id/message', protect, sendMessage);
+console.log('🔄 Registered POST /:id/message route');
 router.put('/:id/read', protect, markAsRead);
+console.log('🔄 Registered PUT /:id/read route');
 
 module.exports = router;
